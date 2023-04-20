dec10eeca6bc75ef4ad870bb9eed1ac6632cb48c
Dwayna Harris <dwaynaharris@gmail.com>
2023-04-19 22:01:30 -0400

    tried to write different first class functions

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    let fare = function () {
+        return x * x;
+    }
+    return fare;
+}
+
+function fareDoubler(x) {
+    return x * 2;
+}
+
+function fareTripler(x) {
+    return x * 3;
+}
+ 
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    if (func = returnFirstTwoDrivers()) {
+        return arrayOfDrivers.slice(0, 2);
+    }else if (func = returnLastTwoDrivers()) {
+        return arrayOfDrivers.slice(2);
+    }
+}
